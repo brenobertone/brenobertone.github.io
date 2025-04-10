6c4c5d7e362a769434b56a2fd6771f522c2e4473
Breno Bertone <bertonebreno@gmail.com>
2025-04-10 20:29:35 -0300

    Quartz sync: Apr 10, 2025, 8:29 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -50,7 +50,7 @@ export const defaultContentPageLayout: PageLayout = {
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
-    Component.DesktopOnly(Component.PageTitlePicture)
+    Component.DesktopOnly(Component.PageTitlePicture()),
     Component.MobileOnly(Component.PageTitle()),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
